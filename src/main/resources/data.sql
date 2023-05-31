618bf41c5d4c45f1194639051292d378fb5791fe
borisavz <borisavzivanovic@gmail.com>
2023-05-31 19:30:57 +0200

    Inicijalizaija baze gur-gur metod

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,20 @@
+DELETE FROM fakultet.smer;
+
+DELETE FROM fakultet.admin_fakulteta;
+DELETE FROM fakultet.fakultet;
+DELETE FROM fakultet.hibernate_sequence;
+DELETE FROM fakultet.student;
+
 INSERT INTO fakultet.admin_fakulteta (id,ime,prezime) VALUES
-	 (1,'Boro','Boro');
+         (1,'Boro','Boro');
 INSERT INTO fakultet.fakultet (id,naziv) VALUES
-	 (1,'FTN'),
-	 (2,'Pravni');
+     (1,'FTN'),
+     (2,'Pravni');
 INSERT INTO fakultet.hibernate_sequence (next_val) VALUES
-	 (4);
+     (4);
 INSERT INTO fakultet.smer (id,naziv,nivo_studija,fakultet_id) VALUES
-	 (1,'SIT',0,1),
-	 (2,'E2',0,1),
-	 (3,'pravo',0,2);
+     (1,'SIT',0,1),
+     (2,'E2',0,1),
+     (3,'pravo',0,2);
 INSERT INTO fakultet.student (id,ime,jmbg,prezime) VALUES
-	 (2,'Gagi','222','Gagi');
+     (2,'Gagi','222','Gagi');
