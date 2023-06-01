8b40028918fa69ac31628c63ec8f7c0d00dc6463
borisavz <borisavzivanovic@gmail.com>
2023-06-01 21:36:47 +0200

    added missing constructors

--- src/main/java/com/borisavz/fakultetback/dto/FakultetDiplomaDTO.java
+++ src/main/java/com/borisavz/fakultetback/dto/FakultetDiplomaDTO.java
@@ -1,15 +1,15 @@
 package com.borisavz.fakultetback.dto;
 
 import com.borisavz.fakultetback.entity.StatusStudija;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.Map;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class FakultetDiplomaDTO {
 
--- src/main/java/com/borisavz/fakultetback/dto/SkolaDiplomaDTO.java
+++ src/main/java/com/borisavz/fakultetback/dto/SkolaDiplomaDTO.java
@@ -1,14 +1,14 @@
 package com.borisavz.fakultetback.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.Map;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class SkolaDiplomaDTO {
 
